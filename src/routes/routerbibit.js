fea7e7fbad20e77c4f3eb409d7c3efc5e1c49b3f
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 21:05:18 +0700

    membuat route untuk bibit

--- src/routes/routerbibit.js
+++ src/routes/routerbibit.js
@@ -1,2 +1,7 @@
 const router = require('express').Router();
 const bibitController = require('../controller/controllerbibit');
+
+router.get('/', bibitController.getbibit);
+router.post('/post', bibitController.postbibit);
+router.post('/update/:id', bibitController.updatebibit);
+router.get('/delete/:id', bibitController.deletebibit);
