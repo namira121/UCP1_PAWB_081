a6df0c629fa5361bc34cf91df82b842b63bfc61e
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 23:12:14 +0700

    membuat tampilan untuk pupuk

--- src/routes/routerpupuk.js
+++ src/routes/routerpupuk.js
@@ -7,7 +7,7 @@ router.post('/post', pupukController.postpupuk);
 router.post('/update/:id', pupukController.updatepupuk);
 router.get('/delete/:id', pupukController.deletepupuk);
 router.get("/", (req, res) => {
-    res.send("Halaman pupuk");
+    res.send("Halaman Pupuk");
 });
 
 router.post("/tambah", (req, res) => {
