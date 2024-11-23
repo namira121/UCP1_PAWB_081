91f14fb3b64585cab12cad520a76b0214b2922c2
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 20:49:32 +0700

    menambahkan post untuk menambahkan data ke tabel bibit

--- src/controller/controllerbibit.js
+++ src/controller/controllerbibit.js
@@ -17,5 +17,33 @@ module.exports = {
             connection.release();
         });
     },
+    postbibit(req, res) {
+        let { nama_bibit, jenis_bibit, jumlah_bibit } = req.body;
+        console.log(nama_bibit, jenis_bibit, jumlah_bibit); 
+        if (nama_bibit && jenis_bibit && jumlah_bibit) {
+            pool.getConnection(function (err, connection) {
+                if (err) throw err;
+                connection.query(
+                    `INSERT INTO bibit (nama_bibit, jenis_bibit, jumlah_bibit) VALUES (?, ?, ?);`,
+                    [nama_bibit, jenis_bibit, jumlah_bibit], 
+                    function (error, results) {
+                        if (error) {
+                            console.error(error);
+                            res.send('Gagal menyimpan data');
+                            return;
+                        }
+                        req.flash('color', 'success');
+                        req.flash('status', 'Yes..');
+                        req.flash('message', 'Data berhasil disimpan');
+                        res.redirect('/bibit');
+                    }
+                );
+                connection.release();
+            });
+        } else {
+            res.send('Data tidak lengkap');
+        }
+    },
+   
         
 }
