87639ee38627b20a04a2713817c99857c5beb5cf
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 23:08:02 +0700

    membuat router untuk pupuk

--- src/routes/routerpupuk.js
+++ src/routes/routerpupuk.js
@@ -1,3 +1,16 @@
-// router.get("/", (req, res) => {
-//     res.send("Halaman Pupuk");
-// });
+const router = require('express').Router();
+const express = require("express");
+const pupukController = require('../controller/controllerpupuk');
+
+router.get('/', pupukController.getpupuk);
+router.post('/post', pupukController.postpupuk);
+router.post('/update/:id', pupukController.updatepupuk);
+router.get('/delete/:id', pupukController.deletepupuk);
+router.get("/", (req, res) => {
+    res.send("Halaman pupuk");
+});
+
+router.post("/tambah", (req, res) => {
+    res.send("pupuk Ditambahkan");
+});
+module.exports = router;
